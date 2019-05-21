cff3c537aad4fc22e1e7efbfe1cb60044484a00c
svensp <sps@ipunkt.biz>
2019-05-21 12:38:07 +0200

    dsn added heartbeat = 30

--- RabbitMQ/config/rabbitmq.php
+++ RabbitMQ/config/rabbitmq.php
@@ -2,4 +2,5 @@
 return [
     'dsn' => 'amqp+ext://'.env('RMQ_USERNAME').':'.env('RMQ_PASSWORD').'@'.
         env('RMQ_HOST', 'rabbitmq').':'.env('RMQ_PORT', 5672).'/'.urlencode(env('RMQ_VHOST', '/'))
+        .'?heartbeat=30'
 ];
