d4654d5b07b006253eeefa7b2f2b764bbb9d4f1a
svensp <sps@ipunkt.biz>
2019-06-25 11:09:27 +0200

    #1 username and password fallback to `guest`

--- RabbitMQ/config/rabbitmq.php
+++ RabbitMQ/config/rabbitmq.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'dsn' => 'amqp+ext://'.env('RMQ_USERNAME').':'.env('RMQ_PASSWORD').'@'.
+    'dsn' => 'amqp+ext://'.env('RMQ_USERNAME', 'guest').':'.env('RMQ_PASSWORD', 'guest').'@'.
         env('RMQ_HOST', 'rabbitmq').':'.env('RMQ_PORT', 5672).'/'.urlencode(env('RMQ_VHOST', '/'))
         .'?heartbeat=30'
 ];
