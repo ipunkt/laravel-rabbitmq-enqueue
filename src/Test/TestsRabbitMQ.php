4d4ff9c86fc94bd157351a8e863816735da36d0d
svensp <sps@ipunkt.biz>
2020-05-28 15:53:34 +0200

    TestsRabbitMQ - added setUpRabbitMQ and expectMessage helpers

--- src/Test/TestsRabbitMQ.php
+++ src/Test/TestsRabbitMQ.php
@@ -34,4 +34,40 @@ trait TestsRabbitMQ
 
         return $message;
     }
+
+    protected function setUpRabbitMQ() {
+        $this->rabbitMQ = Mockery::mock( RabbitMQ::class );
+        $this->rabbitMQ->shouldIgnoreMissing($this->rabbitMQ);
+        $this->app->instance(RabbitMQ::class, $this->rabbitMQ);
+    }
+
+    /**
+     * @param string $event
+     * @param array $expectedData
+     */
+    protected function expectMessage( $event, $expectedData ) {
+        $this->rabbitMQ->shouldReceive( 'onExchange' )->with( 'default-exchange', $event )->once()->andReturnSelf();
+        $this->rabbitMQ->shouldReceive( 'publish' )->withArgs(  function ( $data ) use ( $expectedData ) {
+            $anyMatch = false;
+
+            foreach ( $expectedData as $expectedKey => $expectedValue ) {
+
+                if ( !array_key_exists( $expectedKey, $data ) )
+                    return false;
+
+                if( is_float($data[ $expectedKey ]))
+                    $data[ $expectedKey ] = round($data[ $expectedKey ], 9);
+
+                if( is_float($expectedValue))
+                    $expectedValue = round($expectedValue, 9);
+
+                if ( $data[ $expectedKey ] != $expectedValue )
+                    return false;
+
+                $anyMatch = true;
+            }
+
+            return $anyMatch;
+        } )->once()->andReturnSelf();
+    }
 }
